b1d91ac4ed753e213b93b458d96ffe86c2eb8e59
Fearless <atesaltinkaynak@gmail.com>
2022-12-16 23:06:48 +0300

    v0.0.1
    byqushfan

---
+++ clqu.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'at00m',
+    version: '3.0.0',
+    githubName: 'justat00m',
+    githubKey: "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",
+    softwareerKey: process.env.SOFTWAREER_KEY,
+    email: 'atesaltinkaynak@gmail.com',
+    errors: {
+        404: "This page could not be found.",
+        500: "An error occurred while processing your request."
+    }
+} 
