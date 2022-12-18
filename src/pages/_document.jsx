e729e24f0b7392a63bad1318608032e440c9ff97
Fearless <atesaltinkaynak@gmail.com>
2022-12-18 16:34:07 +0300

    New Update

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -10,7 +10,7 @@ export default function MyDocument() {
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
                 <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
-                <meta name="description" content="16 y/o. Full-stack Developer" />
+                <meta name="description" content="15 y/o. Full-stack Developer" />
                 <link rel="icon" href="https://www.google.com/favicon.ico" type="image/x-icon"/>
             </Head>
             <body>
