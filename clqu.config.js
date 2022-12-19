4302482d4b4f6000d94cf24e74eabd3de2f16ad9
Fearless <atesaltinkaynak@gmail.com>
2022-12-19 23:16:22 +0300

    softwareerupdate

--- clqu.config.js
+++ clqu.config.js
@@ -3,7 +3,7 @@ module.exports = {
     version: '3.0.0',
     githubName: 'justat00m',
     githubKey: "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",
-    softwareerKey: process.env.SOFTWAREER_KEY,
+    softwareerKey: "ab4d3369239a964d240b4f8f0203920cdb5114b2e1e6d66013a94a8b8de0121d2c86b598",
     email: 'atesaltinkaynak@gmail.com',
     errors: {
         404: "This page could not be found.",
