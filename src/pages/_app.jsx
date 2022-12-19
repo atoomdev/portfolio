bb6ada197c44fabbc55497f4e08ffea69f711612
Fearless <atesaltinkaynak@gmail.com>
2022-12-19 21:16:53 +0300

    update

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -16,7 +16,8 @@ function MyApp({ Component, pageProps }) {
       <PageProvider>
         <main>
           <head>
-            <title>at00m | Junior Full-stack Developer</title>
+            <title>at00m | Full-stack Developer</title>
+
           </head>
           <CustomCursor />
           <ContextMenu
