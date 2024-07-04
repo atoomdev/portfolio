316ad8961077018a585f77c3e4a457fdacc880b9
at00m <atesaltinkaynak@gmail.com>
2024-07-04 18:33:30 +0300

    Update clqu.config.js

--- clqu.config.js
+++ clqu.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'at00m',
     version: '3.0.0',
-    githubName: 'justat00m',
+    githubName: 'atoomdev',
     githubKey: "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",
     softwareerKey: "ab4d3369239a964d240b4f8f0203920cdb5114b2e1e6d66013a94a8b8de0121d2c86b598",
     email: 'atesaltinkaynak@gmail.com',
