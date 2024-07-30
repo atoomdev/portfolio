2430bf5f3e3d5e4095df05ba174840567889aba8
at00m <atesaltinkaynak@gmail.com>
2024-07-31 02:06:09 +0300

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -27,9 +27,9 @@ export default function Home() {
         Developer - Entrepreneur
       </p>
       <div className="w-full lg:w-auto grid grid-cols-1 lg:grid-cols-4 gap-4 mt-24">
-  <div className="rounded-lg border-2 border-gray-500/10 border-dotted px-4 py-2 text-center neon-effect">
+  <div className="rounded-lg border-2 border-white-500/10 border-dotted px-4 py-2 text-center neon-effect">
     <span className="text-primary uppercase">Technologies</span>
-    <p className="text-gray-500 dark:text-gray-300 font-semibold text-xl">{skills?.length}13+</p>
+    <p className="text-gray-500 dark:text-gray-300 font-semibold text-xl">13+</p>
   </div>
   <div className="rounded-lg border-2 border-gray-500/10 border-dotted px-4 py-2 text-center neon-effect">
     <span className="text-primary uppercase">Years of Experience</span>
@@ -39,7 +39,7 @@ export default function Home() {
     <span className="text-primary uppercase">Completed Projects</span>
     <p className="text-gray-500 dark:text-gray-300 font-semibold text-xl">30+</p>
   </div>
-  <div className="rounded-lg border-2 border-gray-500/10 border-dotted px-4 py-2 text-center neon-effect">
+  <div className="rounded-lg border-2 border-gray-500/10 border-dotted px-4 py-2 text-center neon-effect fade-in">
     <span className="text-primary uppercase">Repositories</span>
     <p className="text-gray-500 dark:text-gray-300 font-semibold text-xl">{repositories?.length}+</p>
   </div>
