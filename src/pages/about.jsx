384db66baba9531b9e1679b5582f5aae633aa1f8
at00m <atesaltinkaynak@gmail.com>
2024-07-31 02:53:21 +0300

    Update about.jsx

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -33,7 +33,7 @@ export default function About() {
                             </a>
                             <a href={`https://www.linkedin.com/in/ate%C5%9F-alt%C4%B1nkaynak-abb5912a8/`} target="_blank" rel="noreferrer">
                                 <Button className="flex items-center gap-2">
-                                <i class="fa fa-linkedin" aria-hidden="true"></i>
+                                    <i class="fa fa-linkedin" aria-hidden="true"></i>
                                     LinkedIn
                                 </Button>
                             </a>
