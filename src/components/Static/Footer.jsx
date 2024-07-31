dafb2ee9917e5c9324df397405bcbcb225ffd99d
at00m <atesaltinkaynak@gmail.com>
2024-07-31 03:10:58 +0300

    update

--- src/components/Static/Footer.jsx
+++ src/components/Static/Footer.jsx
@@ -1,6 +1,6 @@
 import Button from "components/Global/Button";
 import Link from "next/link";
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
 import { useState } from "react";
 import clquConfig from "../../../config";
 
@@ -18,15 +18,23 @@ export default function Footer() {
         return color;
     }
 
-    return <>
-        <div className="max-w-7xl mx-auto py-12 border-t border-gray-500/10">
-            <div className="flex flex-col lg:flex-row items-center justify-between gap-4">
-                <p className="text-lg text-zinc-400">© 2019 - {new Date().getFullYear()} {clquConfig.name}. All rights reserved.</p>
-                <div className="text-right">
-                    <p className="text-lg text-zinc-400 select-none">Made with <i onClick={() => { setHeartColor(randomColor()) }} className="fas fa-heart" style={{ color: heartColor, cursor: 'pointer' }} /> Thanks To <a href="https://github.com/clqu" className="text-zinc-400 hover:text-primary hover:dark:text-white">clqu</a></p>
-                    <p className="text-sm text-zinc-400 select-none">Powered by <a href="https://sapienties.com" target= "_blank" className="text-zinc-400 hover:text-primary hover:dark:text-white">Light AI</a></p>
+    return (
+        <>
+            <div className="max-w-7xl mx-auto py-12 border-t border-gray-500/10">
+                <div className="flex flex-col lg:flex-row items-center justify-between gap-4">
+                    <p className="text-lg text-zinc-400">
+                        © 2019 - {new Date().getFullYear()} {clquConfig.name}. All rights reserved.
+                    </p>
+                    <div className="text-right">
+                        <p className="text-lg text-zinc-400 select-none">
+                            Made with <i onClick={() => { setHeartColor(randomColor()) }} className="fas fa-heart" style={{ color: heartColor, cursor: 'pointer' }} /> Thanks To <a href="https://github.com/clqu" className="text-zinc-400 hover:text-primary hover:dark:text-white">clqu</a>
+                        </p>
+                        <p className="text-sm text-zinc-400 select-none">
+                            Powered by <a href="https://sapienties.com" target="_blank" className="text-zinc-400 hover:text-primary hover:dark:text-white text-light-ai">Light AI</a>
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
-    </>
+        </>
+    );
 }
